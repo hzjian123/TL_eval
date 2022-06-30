3375507979ff93ccc6dafb0a3fc292be672a0487
fengzhen <fengzhen@haomo.ai>
2022-06-30 11:08:56 +0800

    添加过滤条件

--- src/tf_compare.py
+++ src/tf_compare.py
@@ -2,6 +2,7 @@ from pathlib import Path
 import os
 import sys
 import glob
+from cv2 import copyTo
 # from turtle import width
 import numpy as np
 import argparse
@@ -112,6 +113,116 @@ def read_json(filename):
         bboxs.append(bbox)
     return bboxs
 
+
+def iou_bbox(label_bbox, infer_bbox, thres=0.5):
+    label_bbox_tmp = BBox(label_bbox[0], label_bbox[1], label_bbox[2], label_bbox[3])
+    infer_bbox_tmp = BBox(infer_bbox[0], infer_bbox[1], infer_bbox[2], infer_bbox[3])
+    if iou_calculate(label_bbox_tmp, infer_bbox_tmp) >= thres:
+        return True
+    return False
+
+def read_json_filter_label_infer_bboxs(filename, infer_bboxs_tmp):
+    f = open(filename, encoding='utf-8')
+    file = json.load(f)
+    f.close()
+    objs = file["objects"]
+    width = file["width"]
+    height = file["height"]
+    label_bboxs = []
+    class_name_bboxs = []
+    pose_orientation_bboxs = []
+    toward_orientation_bboxs = []
+    characteristic_bboxs = []
+    detect_filter_bboxs = []
+    truncation_bboxs = []
+    for obj in objs:
+        label_bbox = obj["bbox"]
+        # print(type(label_bbox))
+        if data_set_name == "ICU30":
+            if height == 1080:
+                label_bbox = [round(i * 2) for i in label_bbox]
+            else:
+                label_bbox = [round(i) for i in label_bbox]
+        elif data_set_name == "HD2.0":
+            label_bbox = [round(i) for i in label_bbox]
+        if obj["class_name"] != "traffic_light":
+            class_name_bboxs.append(label_bbox)
+            continue
+        if obj['pose_orientation'] not in [0, 1]:
+            # 非横向和竖向的灯滤掉
+            pose_orientation_bboxs.append(label_bbox)
+            continue
+        if obj['toward_orientation'] not in [0]:  # V18背面标的是1, V21背面标的是2
+            #非正对的灯滤掉
+            toward_orientation_bboxs.append(label_bbox)
+            continue
+        if obj['characteristic'] not in [0, 1]:
+            #非通行灯和行人灯滤掉
+            characteristic_bboxs.append(label_bbox)
+            continue
+        if math.ceil(obj['bbox'][2]) < traffic_light_detect_filter_bbox or math.ceil(obj['bbox'][3]) < traffic_light_detect_filter_bbox:
+            # 任意一边小于10滤掉
+            detect_filter_bboxs.append(label_bbox)
+            continue
+        if obj['truncation'] == 1:
+            #截断的灯滤掉
+            truncation_bboxs.append(label_bbox)
+            continue
+        label_bboxs.append(label_bbox)
+
+    infer_bboxs = []
+    for infer_bbox in infer_bboxs_tmp:
+        class_name_bboxs_flag = False
+        for bbox in class_name_bboxs:
+            if iou_bbox(bbox, infer_bbox) == True:
+                class_name_bboxs_flag = True
+                break
+        if class_name_bboxs_flag == True:
+            continue
+
+        pose_orientation_bboxs_flag = False
+        for bbox in pose_orientation_bboxs:
+            if iou_bbox(bbox, infer_bbox) == True:
+                pose_orientation_bboxs_flag = True
+                break
+        if pose_orientation_bboxs_flag == True:
+            continue
+
+        toward_orientation_bboxs_flag = False
+        for bbox in toward_orientation_bboxs:
+            if iou_bbox(bbox, infer_bbox) == True:
+                toward_orientation_bboxs_flag = True
+                break
+        if toward_orientation_bboxs_flag == True:
+            continue
+
+        characteristic_bboxs_flag = False
+        for bbox in characteristic_bboxs:
+            if iou_bbox(bbox, infer_bbox) == True:
+                characteristic_bboxs_flag = True
+                break
+        if characteristic_bboxs_flag == True:
+            continue
+
+        detect_filter_bboxs_flag = False
+        for bbox in detect_filter_bboxs:
+            if iou_bbox(bbox, infer_bbox) == True:
+                detect_filter_bboxs_flag = True
+                break
+        if detect_filter_bboxs_flag == True:
+            continue
+
+        truncation_bboxs_flag = False
+        for bbox in truncation_bboxs:
+            if iou_bbox(bbox, infer_bbox) == True:
+                truncation_bboxs_flag = True
+                break
+        if truncation_bboxs_flag == True:
+            continue
+
+        infer_bboxs.append(infer_bbox)
+    return label_bboxs, infer_bboxs
+
 def read_txt(filename):
     bboxs = []
     with open(filename, 'r') as fp:
@@ -127,7 +238,7 @@ def read_txt(filename):
             index += 1
     return bboxs
 
-def match_bbox(label_bboxs, infer_bboxs, thres = 4):
+def match_bboxs(label_bboxs, infer_bboxs, thres = 4):
     count = 0
     for bbox in infer_bboxs:
         x = bbox[0]
@@ -150,7 +261,7 @@ def match_bbox(label_bboxs, infer_bboxs, thres = 4):
             count += 1
     return count
 
-def iou_bbox(label_bboxs, infer_bboxs, thres=0.5):
+def iou_bboxs(label_bboxs, infer_bboxs, thres=0.5):
     count = 0
     for bbox in infer_bboxs:
         infer_bbox_tmp = BBox(bbox[0], bbox[1], bbox[2], bbox[3])
@@ -171,8 +282,7 @@ def run(lable_path=input_lable_path, infer_path=input_infer_path):
     infer_count = 0
     match_count = 0
     for infer_file in infer_files:
-        infer_bboxs = read_txt(infer_file)
-        infer_count += len(infer_bboxs)
+        infer_bboxs_tmp = read_txt(infer_file)
         file_basename = os.path.basename(infer_file)
         file_prefix = os.path.splitext(file_basename)[0]
         index = file_prefix.rfind("{a}_".format(a=debug_os))
@@ -182,13 +292,16 @@ def run(lable_path=input_lable_path, infer_path=input_infer_path):
         if not lable_file in label_files:
             print(f"{lable_file} is not exist!")
             exit(0)
-        label_bboxs = read_json(lable_file)
+        #label_bboxs = read_json(lable_file)
+        label_bboxs, infer_bboxs = read_json_filter_label_infer_bboxs(
+            lable_file, infer_bboxs_tmp)
         lable_count += len(label_bboxs)
+        infer_count += len(infer_bboxs)
         # print(label_bboxs)
         if calculate_method == "pixel" :
-            match_count += match_bbox(label_bboxs, infer_bboxs, pixel_debug_thres)
+            match_count += match_bboxs(label_bboxs, infer_bboxs, pixel_debug_thres)
         elif calculate_method == "iou":
-            match_count += iou_bbox(label_bboxs, infer_bboxs, iou_debug_thres)
+            match_count += iou_bboxs(label_bboxs, infer_bboxs, iou_debug_thres)
     TP_TRD = float(match_count)
     FN_TRD = float(lable_count) - TP_TRD
     FP_TRD = float(infer_count) - TP_TRD
